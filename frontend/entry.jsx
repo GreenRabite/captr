b81e4024f967e6270cfe02c14ec7676ed5f6a99b
Aluo <green.rabite@gmail.com>
2018-01-30 01:32:45 -0800

    Nuking all files

--- frontend/entry.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<h1>captr: the journey begins</h1>, root);
-});
