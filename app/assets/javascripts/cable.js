66452d07aa6716a8323861278ad6655503a0baa2
Aluo <green.rabite@gmail.com>
2018-03-11 21:47:43 -0700

    Initial commit for websockets

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,15 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+export const setSocket = () => (dispatch) => {
+  return App.cable.subscriptions.create({
+    channel: 'GlobalChannel'
+  }, {
+    connected: () => {console.log(`connected to GlobalChannel`);},
+    disconnected: () => {console.log(`disconnected from GlobalChannel`);},
+    received: (data) => {
+      dispatch(parseMessage(data.message));
+    }
+  });
+};
