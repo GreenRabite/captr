95c965685f415702f834bdd6ce1a0d5157b321b1
Aluo <green.rabite@gmail.com>
2018-03-12 01:02:53 -0700

    commented out ActionCable verbage for now to allow heroku to run properly

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,20 +1,20 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_verified_user
-    end
-    
-    private
-    def find_verified_user
-      token = cookies.encrypted[:session_token]
-      if token && verified_user = User.find_by(session_token: token)
-        verified_user
-      else
-        reject_unauthorized_connection
-      end
-    end
+    # identified_by :current_user
+    #
+    # def connect
+    #   self.current_user = find_verified_user
+    # end
+    #
+    # private
+    # def find_verified_user
+    #   token = cookies.encrypted[:session_token]
+    #   if token && verified_user = User.find_by(session_token: token)
+    #     verified_user
+    #   else
+    #     reject_unauthorized_connection
+    #   end
+    # end
 
   end
 end
